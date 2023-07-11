9c006bb3712ae9faef9fc3b71901d11815432efa
NoeRamirezElvir <noe.ramirezjcv.edu.hn>
2023-07-11 00:09:38 -0600

    r5-c2

---
+++ gestion_usuarios/views/datos_permisos_views.py
@@ -0,0 +1,24 @@
+from django.http.response import JsonResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
+import re
+from ..models import *
+from django.views.decorators.http import require_http_methods
+
+@method_decorator(require_http_methods(['POST','PUT','GET','DELETE']), name='dispatch')
+class DatosPermisos(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        usuarios = Usuario.objects.filter(sesion=1).select_related('idEmpleado')
+        if(usuarios):
+            empleados = list(Empleado.objects.filter(id=usuarios[0].idEmpleado.id).values())
+        cargo = list(CargoEmpleado.objects.filter(id=empleados[0].idEmpleado))
+        permisos = Permisos.objects.filter(idCargoEmpleado=cargo[0].id)
+
+        return JsonResponse(permisos)
