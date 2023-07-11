038305088b226b7fe43cbb0f6e570c53b12d0e3f
NoeRamirezElvir <noe.ramirezjcv.edu.hn>
2023-07-11 02:30:55 -0600

    r1-c016

--- gestion_usuarios/views/datos_permisos_views.py
+++ gestion_usuarios/views/datos_permisos_views.py
@@ -13,11 +13,33 @@ class DatosPermisos(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-    
+
     def get(self, request):
         usuarios = Usuario.objects.filter(sesion=1).select_related('idEmpleado')
-        if(usuarios):
+        if usuarios:
             empleados = list(Empleado.objects.filter(id=usuarios[0].idEmpleado.id).values())
+        permisos = Permisos.objects.filter(idCargoEmpleado=empleados[0]['idCargoEmpleado_id']).select_related('idAcciones','idCargoEmpleado','idPantallas')
 
-        data = {'permisos':empleados}
-        return JsonResponse(data)
+        output = {"permisos": []}
+        acciones_por_cargo = {}
+        for permiso in permisos:
+            id_cargo_empleado = permiso.idCargoEmpleado.nombre
+            nombre_pantalla = permiso.idPantallas.nombre
+            id_accion = permiso.idAcciones.nombre
+
+            if id_cargo_empleado in acciones_por_cargo:
+                acciones_pantalla = acciones_por_cargo[id_cargo_empleado]
+                if nombre_pantalla in acciones_pantalla:
+                    acciones_pantalla[nombre_pantalla].append(id_accion)
+                else:
+                    acciones_pantalla[nombre_pantalla] = [id_accion]
+            else:
+                acciones_por_cargo[id_cargo_empleado] = {nombre_pantalla: [id_accion]}
+
+        for id_cargo_empleado, acciones_pantalla in acciones_por_cargo.items():
+            output["permisos"].append({
+                "idCargoEmpleado": id_cargo_empleado,
+                "idPantallas": acciones_pantalla
+            })
+
+        return JsonResponse(output)
