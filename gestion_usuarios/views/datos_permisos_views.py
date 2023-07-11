d47f0dbb3e3119ef10e855acaa7cb5c55a447e03
NoeRamirezElvir <noe.ramirezjcv.edu.hn>
2023-07-11 01:38:31 -0600

    r1-c014

--- gestion_usuarios/views/datos_permisos_views.py
+++ gestion_usuarios/views/datos_permisos_views.py
@@ -19,7 +19,7 @@ class DatosPermisos(View):
         if(usuarios):
             empleados = list(Empleado.objects.filter(id=usuarios[0].idEmpleado.id).values())
 
-        cargo = list(CargoEmpleado.objects.filter(id=empleados[0].idCargoEmpleado.id).values())
+        cargo = list(CargoEmpleado.objects.filter(id=empleados[0].idCargoEmpleado_id).values())
 
         data = {'permisos':cargo}
         return JsonResponse(data)
